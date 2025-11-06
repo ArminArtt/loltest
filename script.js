344cb2314914bdcaa1d5d0ea7b8f73e365ef95f0
vjvc <154592225+ArminArtt@users.noreply.github.com>
2025-11-06 09:34:53 -1200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function copyText(text) {
-  navigator.clipboard.writeText(text).then(() => {
-    const msg = document.getElementById("copyMsg");
-    msg.textContent = `"${text}" copied to clipboard!`;
-    setTimeout(() => msg.textContent = "", 2200);
-  });
-}
